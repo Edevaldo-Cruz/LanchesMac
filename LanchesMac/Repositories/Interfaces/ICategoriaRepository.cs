98813fd9c6f92f7750ccb50b232e1d61bc34a743
Edevaldo Cruz <edevaldo_cruz@hotmail.com>
2022-06-13 18:37:37 -0300

    Adicionar arquivos de projeto.

---
+++ LanchesMac/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,16 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        /*
+            Defini um contrato de interface:
+                IEnumerable gera uma coleção de objetos
+                Categoria fornece as informações    
+                Categorias é o retorno da informações.
+         */
+        IEnumerable<Categoria> Categorias { get; }
+
+    }
+}
---
+++ LanchesMac/Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,16 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        // Retorna todos os lanches
+        IEnumerable<Lanche> Lanches { get; }
+
+        //Retorna todos os lanches definidos como preferidos
+        IEnumerable<Lanche> LanchesPreferidos { get; }
+
+        // Retorna lanches pelo Id
+        Lanche GetLanchesById(int lancheId);
+    }
+}
